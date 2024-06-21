7d4c7795d2a6a2c4d1509ba744980158b6355932
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2024-06-21 23:09:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install PHP
+apt-get update && apt-get install -y php-cli curl
+
 # Install Composer dependencies
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install
