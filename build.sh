81fdaa17e25b2f27c39c480556d914a7bac29887
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2024-06-21 23:01:02 +0700

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Composer dependencies
-curl -sS https://getcomposer.org/installer | php
-php composer.phar install
-
-# Install Node.js dependencies and build assets
-npm install
-npm run build
